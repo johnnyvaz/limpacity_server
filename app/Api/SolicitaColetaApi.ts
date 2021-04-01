9f0f14c0d555a76d9ef1f4a6f9f7e810a599490a
johnnyvaz1 <johnny.ferreira@luizalabs.com>
2021-03-31 23:18:52 -0300

    feat: add env url

--- app/Api/SolicitaColetaApi.ts
+++ app/Api/SolicitaColetaApi.ts
@@ -1,11 +1,14 @@
 const axios = require('axios');
+import Env from '@ioc:Adonis/Core/Env'
 
+const producerUrl = Env.get('PRODUCER_URL');
 export default class SolicitaColetaApi {
+
 
   async postSolicita({ request }){
     const req = request;
        try {
-      const res = await axios.post('http://localhost:9002/producer/api/v1/coleta', req)
+      const res = await axios.post(producerUrl, req)
       return res.data
     } catch (error) {
       error.Request
