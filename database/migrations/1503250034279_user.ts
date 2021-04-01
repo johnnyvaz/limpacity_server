c3e8ca830d9ea777c16baa9d00af208852929cce
johnnyvaz1 <johnny.ferreira@luizalabs.com>
2021-03-31 23:40:56 -0300

    feat: migration user

---
+++ database/migrations/1503250034279_user.ts
@@ -0,0 +1,18 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class User extends BaseSchema {
+  protected tableName = 'users'
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('username', 80).notNullable().unique()
+      table.string('email', 254).notNullable().unique()
+      table.string('password', 60).notNullable()
+      table.timestamps(true)
+    })
+  }
+
+  public async down () {
+    this.schema.dropSchema(this.tableName)
+  }
+}
---
+++ database/migrations/1503250034280_token.ts
@@ -0,0 +1,19 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Tokens extends BaseSchema {
+  protected tableName = 'tokens'
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('user_id').unsigned().references('id').inTable('users')
+      table.string('token', 255).notNullable().unique().index()
+      table.string('type', 80).notNullable()
+      table.boolean('is_revoked').defaultTo(false)
+      table.timestamps()
+    })
+  }
+
+  public async down () {
+    this.schema.dropSchema(this.tableName)
+  }
+}
